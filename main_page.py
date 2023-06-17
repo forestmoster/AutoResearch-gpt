860bef973eaf10b94a8b4421d487b4e224daa327
Administrator <w87618555@gmail.com>
2023-06-18 00:57:12 +0800

    新增了文件

--- main_page.py
+++ main_page.py
@@ -71,6 +71,7 @@ def query_page():
         del st.session_state['回答内容']
     if t.button('重新开始一个回答,当前次数{}'.format(st.session_state['回答次数'])):
         del st.session_state['回答内容']
+        st.session_state['回答内容'] = []
         st.write(st.session_state)
 
 
