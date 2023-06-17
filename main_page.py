f5444a79cc87f9c7d8a73c3fa675f20616a8b4e7
Administrator <w87618555@gmail.com>
2023-06-17 23:54:26 +0800

    新增了文件

--- main_page.py
+++ main_page.py
@@ -71,16 +71,19 @@ def query_page():
         del st.session_state['回答内容']
     if t.button('重新开始一个回答,当前次数{}'.format(st.session_state['回答次数'])):
         del st.session_state['回答内容']
+    with st.form("chat_input", clear_on_submit=True):
+        a, b = st.columns([4, 1])
+        query=t.text_area('请输入500字以内提示语，最多连续提问{}次'.format(count_pass))
+        b.form_submit_button("Send", use_container_width=True)
 
-    query = t.text_area('请输入500字以内提示语，最多连续提问{}次'.format(count_pass))
+    # query = t.text_area('请输入500字以内提示语，最多连续提问{}次'.format(count_pass))
     query_response = query + response1_str
-    with t.button("第{}次提交".format(st.session_state['回答次数'])):
+    if query:
         response = ask_page.ask(query=query_response, model=GPT_MODEL, token_budget=2000 - 500)
         st.write(response)
         response1.append(response)
         st.session_state['回答次数']=st.session_state['回答次数'] + 1
     st.write(':yellow[历史回答：]', st.session_state['回答内容'])
-
 
 
 
