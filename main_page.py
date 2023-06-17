f026554a4203f0cf5897075582baa2602cf4011b
Administrator <w87618555@gmail.com>
2023-06-18 00:16:59 +0800

    新增了文件

--- main_page.py
+++ main_page.py
@@ -71,10 +71,11 @@ def query_page():
         del st.session_state['回答内容']
     if t.button('重新开始一个回答,当前次数{}'.format(st.session_state['回答次数'])):
         del st.session_state['回答内容']
-    st.text('请输入500字以内提示语，最多连续提问{}次'.format(count_pass))
+
     with st.form("chat_input", clear_on_submit=True):
         a, b = st.columns([4, 1])
-        query=a.text_area('请输入500字以内提示语，最多连续提问{}次'.format(count_pass),label_visibility="collapsed",max_chars=500)
+        query=a.text_input('请输入500字以内提示语，最多连续提问{}次'.format(count_pass),
+                           '请输入500字以内提示语，最多连续提问{}次'.format(count_pass),label_visibility="collapsed",max_chars=500)
         b.form_submit_button("Send", use_container_width=True)
 
     # query = t.text_area('请输入500字以内提示语，最多连续提问{}次'.format(count_pass))
