eb8a2a5f0762949cda50c8da5a92f140bf0c353d
Administrator <w87618555@gmail.com>
2023-06-18 11:02:31 +0800

    新增了文件

--- pages/robot_page.py
+++ pages/robot_page.py
@@ -54,7 +54,7 @@ with st.form("chat_input", clear_on_submit=True):
     b.form_submit_button("Send", use_container_width=True)
 
 for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"]=="user" )
+    message(msg["content"], is_user=msg["role"] )
 #
 if user_input and not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
