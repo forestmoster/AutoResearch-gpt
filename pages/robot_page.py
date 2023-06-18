32f4627f4f0245d7af5296d1a2cfe0fd3d96a98c
Administrator <w87618555@gmail.com>
2023-06-18 16:17:29 +0800

    新增了文件

--- pages/robot_page.py
+++ pages/robot_page.py
@@ -15,7 +15,8 @@ if "回答内容"not in st.session_state:
     st.session_state["回答内容"] = [{"role": "assistant", "content": "你好，同学，你想问什么？"}]
 if '回答次数' not in st.session_state:
     st.session_state['回答次数'] = 1
-if st.button('重新开始一个回答,当前次数{}'.format(st.session_state['回答次数'])):
+
+with st.button('重新开始一个回答,当前次数{}'.format(st.session_state['回答次数'])):
     del st.session_state['回答内容']
     del st.session_state['messages']
     st.session_state["回答内容"] = [{"role": "assistant", "content": "你好，同学，你想问什么？"}]
