dcc89812e4e2f14d7b26212379c99fe8668db2e8
Administrator <w87618555@gmail.com>
2023-06-19 11:16:28 +0800

    新增了文件

--- game.py
+++ game.py
@@ -62,6 +62,7 @@ if user_input and not openai_api_key:
 if user_input and openai_api_key:
     openai.api_key = openai_api_key
     st.session_state["messages_game"].append({"role": "user", "content": user_input})
+    st.session_state["回答内容_game"].append({"role": "user", "content": user_input})
     message(user_input, is_user=True)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages= st.session_state["回答内容_game"])
     msg = response.choices[0].message
