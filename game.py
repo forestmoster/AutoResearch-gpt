2e313d27f4775ab6426ef662e43edf8e2af26637
Administrator <w87618555@gmail.com>
2023-06-19 10:40:47 +0800

    新增了文件

--- game.py
+++ game.py
@@ -12,11 +12,11 @@ st.title("💬 烟台南山学院冒险游戏")
 st.caption('是2023年9月的刚进学校的新生，无意间发现了烟台南山学院关于外星人的秘密，让我们开始探索吧！！！')
 # openai.api_key = st.secrets.openai_api_key
 if "messages" not in st.session_state:
-    st.session_state["messages_game"] = [{"role": "assistant", "content":"现在选择一个专业，让我们开始冒险吧！！"}]
+    st.session_state["messages_game"] = [{"role": "assistant", "content":"现在选择一个校区和学院，让我们开始冒险吧！！！"}]
 if "回答内容"not in st.session_state:
     st.session_state["回答内容_game"] = [{"role": "assistant", "content": "现在让我们我玩一个游戏,主人公是2023年9月的刚进学校的还没有选专业的新生，无意中发现了隐藏在学校后面的有关外星人的惊人秘密。"
-                                                                     "现在开始选择专业并且进行冒险"
-                                                                     "你提供两个选项，我来选，然后进行回答。"
+                                                                     "现在开始选择校区和专业并且进行冒险"
+                                                                     "你提供选项，我来选，然后进行回答。"
                                                                      "故事背景是烟台南山学院，烟台南山学院有三个校区，东海校区（东海校区靠海，面积比较大，科技与数据学院、智能科学与工程学院、"
                                                                      "材料科学与工程学院、纺织与服装学院、化学工程与技术学院、航空科学与工程学院、健康学院、经济与管理学院，马克思主义学院。），"
                                                                      "南山校区（在音乐校区山脚下，国学与外语学院、艺术与设计学院），"
@@ -34,11 +34,11 @@ with st.form("chat_input", clear_on_submit=True):
 if st.button('重新开始一个冒险'):
     del st.session_state["回答内容_game"]
     del st.session_state["messages_game"]
-    st.session_state["messages_game"] = [{"role": "assistant", "content":"现在选择一个专业，让我们开始冒险吧！！！"}]
+    st.session_state["messages_game"] = [{"role": "assistant", "content":"现在选择一个校区和学院，让我们开始冒险吧！！！"}]
     st.session_state["回答内容_game"] = [{"role": "assistant", "content": "现在让我们我玩一个游戏,主人公是2023年9月的刚进学校的还没有选专业的新生，无意中发现了隐藏在学校后面的有关外星人的惊人秘密。"
-                                                                     "现在开始选择专业并且进行冒险"
-                                                                     "你提供两个选项，我来选，然后进行回答。"
-                                                                     "故事背景是烟台南山学院，烟台南山学院有三个校区，东海校区（东海校区靠海，面积比较大，科技与数据学院、智能科学与工程学院、"
+                                                                     "现在开始选择校区和专业并且进行冒险"
+                                                                     "你提供选项，我来选，然后进行回答。"
+                                                                     "烟台南山学院有三个校区，东海校区（东海校区靠海，面积比较大，科技与数据学院、智能科学与工程学院、"
                                                                      "材料科学与工程学院、纺织与服装学院、化学工程与技术学院、航空科学与工程学院、健康学院、经济与管理学院，马克思主义学院。），"
                                                                      "南山校区（在音乐校区山脚下，国学与外语学院、艺术与设计学院），"
                                                                      "音乐校区（在山上，靠近南山旅游景区，音乐与舞蹈学院)。"}]
@@ -64,3 +64,4 @@ if user_input and openai_api_key:
     st.session_state.messages_game.append(msg)
     st.session_state["回答内容_game"].append(msg)
     message(msg.content)
+ st.session_state
