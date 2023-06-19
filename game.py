ece653747d0afab5eb7b713f387d253ecec1b4a3
Administrator <w87618555@gmail.com>
2023-06-19 23:32:46 +0800

    新增了文件

--- game.py
+++ game.py
@@ -30,7 +30,7 @@ styl = """
      @media (max-width: 600px) {
             /* 当屏幕宽度小于等于600px时，改变输入框和按钮的样式和位置 */
             .stTextInput {
-                right：100; /* 缩小输入文本框的宽度 */
+                width：100; /* 缩小输入文本框的宽度 */
             }
             
             .stButton {
