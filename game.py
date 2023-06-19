7675dce9dbd395c3ef6c99154ca0d13e89594de5
Administrator <w87618555@gmail.com>
2023-06-19 22:33:00 +0800

    新增了文件

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ styl = """
         width: auto; /* 自适应宽度 */
         max-width: 80%; /* 设置输入框的最大宽度 */
         max-height: 200px; /* 设置输入框的最大高度 */
+        left: 10%; /* 设置输入框距离页面左边的最小距离 */
         z-index: 2;
         left:5rem;
 
