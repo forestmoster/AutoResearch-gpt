63ea38caa75cf1cf5079a6a9b7a7d4ceb08d04cd
Administrator <w87618555@gmail.com>
2023-06-20 10:57:58 +0800

    新增了文件

--- game.py
+++ game.py
@@ -42,7 +42,7 @@ styl = """
             width: 100%;       
             bottom:0rem; 
             z-index:3; 
-            height: 13rem; /* 设置输入框在移动设备上的高度 */
+            height: 140px; /* 设置输入框在移动设备上的高度 */
         }          
         } 
 
