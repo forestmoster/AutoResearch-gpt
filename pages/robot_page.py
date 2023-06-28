9d94b8ae5455f46bc03b091468e3ac1b55927efa
Administrator <w87618555@gmail.com>
2023-06-28 21:56:36 +0800

    新增了文件

--- pages/robot_page.py
+++ pages/robot_page.py
@@ -58,9 +58,26 @@ if user_input :
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.session_state['回答内容'].append(msg)
+    st.write(st.session_state['回答内容'])
     st.session_state['回答次数'] = st.session_state['回答次数'] + 1
     # 修改 st.session_state['回答内容'] 中的最后一条消息的内容
     st.session_state['回答内容'][-2]["content"] = user_input
     message(msg.content)
+
+
+    conversation_string = ""
+    short_state_num = len(st.session_state["回答内容"])
+
+    start_round = int(short_state_num * 3 / 10)
+    end_round = int(short_state_num * 7 / 10)
+    for i in range(short_state_num):
+        conversation_string += st.session_state["回答内容"][i]["content"] + "\n"
+    # 调用计算文字的函数
+    conversation_string_num = len(conversation_string)
+    st.write( conversation_string_num)
+    if conversation_string_num > 2000 or st.session_state['回答次数'] > 6:
+        del st.session_state["回答内容"][start_round: end_round]
+        st.session_state['回答次数'] = 1
+
 
 
