22998d967cb8a74fdf07f49320a3a407ca953d6b
Administrator <w87618555@gmail.com>
2023-08-06 20:20:23 +0800

    增加了上传和读取文件的功能

--- pages/外文论文助手.py
+++ pages/外文论文助手.py
@@ -1,7 +1,7 @@
 
-# import sys
-# __import__("pysqlite3")
-# sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+import sys
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 
 import ast
